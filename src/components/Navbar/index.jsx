5ffe1ee92063448892af9965ee62002d62d1691c
miltondw <estradamilton2001@gmail.com>
2024-03-13 17:52:49 -0500

    fix al hacer click en detail, no cerraba el checkout; se agregaron distintos estilos para el diseño responsive

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,124 +1,112 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { ShoppingCartContext } from "../../Context";
-import { ShoppingBagIcon } from "@heroicons/react/24/solid";
+import {
+  Bars3BottomLeftIcon,
+  ShoppingBagIcon,
+} from "@heroicons/react/24/solid";
 import useLocalStorage from "../../hooks/useLocalStorage";
+
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   const context = useContext(ShoppingCartContext);
   const login = useLocalStorage("login");
-
-  const menu1 = [
-    {
-      to: "/",
-      text: "Shopi",
-      className: "font-semibold text-lg",
-    },
-    {
-      to: "/",
-      text: "All",
-    },
-  ];
-
-  const menu2 = [
-    {
-      to: "/email",
-      text: context.userData?.email,
-      className: "text-black/60",
-    },
-    {
-      to: "/my-orders",
-      text: "My orders",
-    },
-    {
-      to: "/my-account",
-      text: "My account",
-    },
-    {
-      to: "/sign-in",
-      text: "Sign Out",
-      click: () => sinup(),
-    },
-    {
-      to: "/shoppcar",
-      text: <ShoppingBagIcon className="h-6 w-6 text-black"></ShoppingBagIcon>,
-    },
-  ];
-
-  const menu3 = [
-    {
-      to: "/sign-in",
-      text: !context.userData ? "Sign Up" : "Sign In",
-    },
-  ];
+  const activeStyle = "underline underline-offset-4";
   const sinup = () => {
     login.saveItems(false);
     context.setLogin(false);
   };
-
-  const activeStyle = " underline underline-offset-4";
   return (
-    <nav className="flex w-full justify-between items-center fixed z-10 top-0 py-5 px-8 text-sm font-light bg-white/80">
-      <ul className="flex items-center gap-3 capitalize">
-        {menu1.map((item, i) => (
-          <li key={i} className={"font-semibold text-lg"}>
-            <NavLink
-              to={item.to}
-              onClick={() => context.setSearchByCategory("")}
-            >
-              {item.text}
+    <>
+      <button
+      type="button"
+        onClick={toggleMenu}
+        className="text-black p-2 focus:outline-none fixed top-0 bg-white/80 z-10 w-full lg:hidden "
+      >
+        <Bars3BottomLeftIcon className="h-7 w-7 text-gray-500 active:scale-x-125 " />
+      </button>
+      <nav
+        onClick={toggleMenu}
+        className={`
+      flex justify-between items-center fixed z-10 top-0 py-5 px-8 text-sm font-light bg-white/80 
+      mt-11 flex-col w-auto lg:w-full lg:flex-row lg:mt-0 lg:translate-x-0
+      transition-transform duration-300 ${
+        menuOpen ? "translate-x-0" : "-translate-x-full"
+      }
+      `}
+      >
+        <ul className="flex items-center gap-3 capitalize flex-col lg:flex-row">
+          {/* Menu 1 */}
+          <li className="font-semibold text-lg">
+            <NavLink to="/" onClick={() => context.setSearchByCategory("")}>
+              Shopi
             </NavLink>
           </li>
-        ))}
-        {context.itemsCategorys?.map((item, i) => (
-          <li key={i}>
-            <NavLink
-              to={`/${item}`}
-              className={({ isActive }) => (isActive ? activeStyle : "")}
-              onClick={() => context.setSearchByCategory(item)}
-            >
-              {item}
+          <li>
+            <NavLink to="/" onClick={() => context.setSearchByCategory("")}>
+              All
             </NavLink>
           </li>
-        ))}
-      </ul>
-      <ul className="flex items-center gap-3">
-        {!context.loginState
-          ? menu3.map((item, i) => (
-              <li key={i}>
-                <NavLink
-                  to={item.to}
-                  className={({ isActive }) => (isActive ? activeStyle : "")}
-                >
-                  {item.to == "/shoppcar" ? (
-                    <div className="flex gap-2 items-center">
-                      <ShoppingBagIcon className="h-6 w-6 text-black" />
-                      {context.cartProducts.length}
-                    </div>
-                  ) : (
-                    item.text
-                  )}
+          {/* Menu 2 */}
+          {context.itemsCategorys?.map((item, i) => (
+            <li key={i}>
+              <NavLink
+                to={`/${item}`}
+                className={({ isActive }) =>
+                  isActive ? activeStyle : undefined
+                }
+                onClick={() => context.setSearchByCategory(item)}
+              >
+                {item}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <ul className="flex items-center gap-3 flex-col lg:flex-row">
+          {/* Menu 3 */}
+          {!context.loginState ? (
+            <li>
+              <NavLink to="/sign-in">
+                {context.userData ? "Sign In" : "Sign Up"}
+              </NavLink>
+            </li>
+          ) : (
+            <>
+              <li className="text-black/60">
+                {context.userData?.email.split("@")[0]}
+              </li>
+              <li>
+                <NavLink to="/my-orders">My orders</NavLink>
+              </li>
+              <li>
+                <NavLink to="/my-account">My account</NavLink>
+              </li>
+              <li>
+                <NavLink to="/sign-in" onClick={sinup}>
+                  Sign Out
                 </NavLink>
               </li>
-            ))
-          : menu2.map((item, i) => (
-              <li key={i} className={i == 0 ? "text-black/60" : ""}>
-                <NavLink
-                  to={item.to}
-                  className={({ isActive }) => (isActive ? activeStyle : "")}
-                  onClick={item?.click}
+              <li>
+                <div
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    context.openCheckoutSideMenu();
+                    toggleMenu()
+                  }}
+                  className="flex gap-2 items-center"
                 >
-                  {item.to == "/shoppcar" ? (
-                    <div className="flex gap-2 items-center">
-                      <ShoppingBagIcon className="h-6 w-6 text-black" />
-                      {context.cartProducts.length}
-                    </div>
-                  ) : (
-                    item.text
-                  )}
-                </NavLink>
+                  <ShoppingBagIcon className="h-6 w-6 text-black" />
+                  {context.cartProducts.length}
+                </div>
               </li>
-            ))}
-      </ul>
-    </nav>
+            </>
+          )}
+        </ul>
+      </nav>
+    </>
   );
 };
--- src/components/OrderCard/index.jsx
+++ src/components/OrderCard/index.jsx
@@ -8,7 +8,7 @@ const OrderCard = ({ id,title, imageUrl, price,handleDelete  }) => {
     renderXMarkIcon = <XMarkIcon onClick={() => handleDelete(id)} className='h-6 w-6 text-black cursor-pointer'></XMarkIcon>
   }
   return (
-    <div className="flex justify-between items-center mb-3">
+    <div className="flex justify-between items-center mb-3 flex-col-reverse border-transparent border-b-gray-500 border p-1 lg:flex-row lg:border-none">
       <div className="flex items-center gap-2">
         <figure className="w-20 h-20">
           <img
@@ -23,8 +23,8 @@ const OrderCard = ({ id,title, imageUrl, price,handleDelete  }) => {
         </figure>
         <p className="text-sm font-light">{title}</p>
       </div>
-      <div className="flex items-center gap-2">
-        <p className="text-lg font-medium">{price}</p>
+      <div className="flex items-center gap-2 w-full justify-between lg:w-auto lg:justify-normal">
+        <p className="text-lg font-medium">${price}</p>
         {renderXMarkIcon}
       </div>
     </div>
